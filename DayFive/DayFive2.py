ff0c3d2b65c8b8c76e265ab88f133878c68a1914
PrensesMTN <60544998+PrensesMTN@users.noreply.github.com>
2023-07-25 23:58:16 +0300

    Update DayFive2.py

--- DayFive/DayFive2.py
+++ DayFive/DayFive2.py
@@ -1,9 +1,18 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
+#import data
 df = pd.read_csv('data.csv')
 
+#find meian and replace  to null calories cells
+x = df["Calories"].median()
+
+df["Calories"].fillna(x, inplace = True)
+
+
+#plot to data 
 df.plot()
+
+df.plot(kind = 'scatter', x = 'Duration', y = 'Calories')
 
 plt.show()
