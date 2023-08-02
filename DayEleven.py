d3f22a98dd7b8a1e5ac3a5544b9d3754b9adf840
PrensesMTN <60544998+PrensesMTN@users.noreply.github.com>
2023-08-02 22:56:19 +0300

    Update DayEleven.py

--- DayEleven.py
+++ DayEleven.py
@@ -31,106 +31,6 @@ def get_index2(*arg):
 
 
 
-def calc():
-    if entry.get() :
-        # **************** fah to other termo values ****************
-        if get_index1() == "0": 
-            if get_index2()=="0":
-                f = int(entry.get())
-                lbl_res["text"] = f"Results: {f} °F"  
-                print(f)
-            elif get_index2()=="1":
-                c=int(float(entry.get())-32)/9*5
-                lbl_res["text"] = f"Results: {c} °C"  
-                print(c)
-
-            elif get_index2()=="2":
-                r=int(float(entry.get())+459.67)
-                lbl_res["text"] = f"Results: {r} °R"  
-                print(r)
-
-            elif get_index2()=="3":
-                k=int(float(entry.get())+459.67)/9*5
-                lbl_res["text"] = f"Results: {k} °K"  
-                print(k)
-
-
-        # **************** celcius to other termo values ****************
-        if get_index1() == "1":
-            if get_index2()=="0":
-                f = int(float(entry.get())) *9/5+32
-                lbl_res["text"] = f"Results: {f} °F"  
-                print(f)
-            elif get_index2()=="1":
-                c = int(entry.get())
-                lbl_res["text"] = f"Results: {c} °C"  
-                print(c)
-
-            elif get_index2()=="2":
-                r=int(float(entry.get())) / 9*5 + 491.67
-                lbl_res["text"] = f"Results: {r} °R"  
-                print(r)
-
-            elif get_index2()=="3":
-                k=int(float(entry.get())+273.15)            
-                lbl_res["text"] = f"Results: {k} °K"  
-                print(k)
-
-
-        # **************** Reaumur to other termo values ****************
-        if get_index1() == "2":
-            if get_index2()=="0":
-                fah = int(float(entry.get()))-459.67
-                lbl_res["text"] = f"Results: {fah} °F"  
-                print(fah)
-            elif get_index2()=="1":
-                c=int(float(entry.get())) / 9*5 -273.15
-                lbl_res["text"] = f"Results: {c} °C"  
-                print(c)
-
-            elif get_index2()=="2":
-                r = int(entry.get())
-                lbl_res["text"] = f"Results: {r} °R"  
-                print(r)
-
-            elif get_index2()=="3":
-                k=int(float(entry.get()))/5*9
-                lbl_res["text"] = f"Results: {k} °K"  
-                print(k)
-
-
-
-        # **************** kelvin to other termo values ****************
-        if get_index1() == "3":
-            if get_index2()=="0":
-                f = int(float(entry.get()))/5*9- 459.67
-                lbl_res["text"] = f"Results: {f} °F"  
-                print(f)
-            elif get_index2()=="1":
-                c=int(float(entry.get())) - 273.15
-                lbl_res["text"] = f"Results: {c} °C"  
-                print(c)
-
-            elif get_index2()=="2":
-                r=int(float(entry.get()))/5*9
-                lbl_res["text"] = f"Results: {r} °R"  
-                print(r)
-
-            elif get_index2()=="3":
-                k = int(entry.get())
-                lbl_res["text"] = f"Results: {k} °K"  
-                print(k)
-    else:
-        messagebox.showerror("Error", "Please enter a valid temperature value")
-
-varCombo1 = tk.StringVar()
-
-varCombo2 = tk.StringVar()
-
-tempCombo1 = ttk.Combobox(win, width = 27, textvariable = varCombo1)
-tempCombo2 = ttk.Combobox(win, width = 27, textvariable = varCombo2)
-
-
 tempCombo1['values'] = ('Fahrenheit (Fahrenhayt)(°F)',
                           ' Santigrat (Celcius)(°C) ',
                           ' Reaumur (Reomür)(°R) ',
@@ -164,10 +64,6 @@ btn.grid(row=3, column=5,padx=30)
 
 btn_clr = tk.Button(text="Clear", command=clear,font=25)
 btn_clr.grid(row=3, column=1)
-
-to_lbl = tk.Label(text="To:",font=25)
-to_lbl.grid(row=0,column=2, padx=100,pady=15)
-temperature.grid(row=2, column=2,pady=5)
 
 
 lbl_res = tk.Label(text="Results:",font=5)
